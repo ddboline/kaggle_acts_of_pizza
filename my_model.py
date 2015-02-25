47ffe1d75bdc595af5b0e26621bb4088d5c2bc73
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-25 17:02:51 -0500

    ...

--- my_model.py
+++ my_model.py
@@ -19,6 +19,8 @@ from sklearn.decomposition import PCA
 
 from sklearn.metrics import roc_auc_score
 
+from sklearn.feature_extraction.text import CountVectorizer
+
 def review_to_wordlist(review, remove_stopwords=False):
     # Function to convert a document to a sequence of words,
     # optionally removing stop words.  Returns a list of words.
@@ -42,11 +44,16 @@ def review_to_wordlist(review, remove_stopwords=False):
     # 5. Return a list of words
     return(words)
 
+def clean_review_function(review):
+    global master_word_dict, number_of_rows
+    list_of_words = review_to_wordlist(review, remove_stopwords=False)
+    return ' '.join(list_of_words)
+
 
 def load_data():
     train_df = pd.read_json('train.json')
-    test_df = pd.read_json('test.json')
     subm_df = pd.read_csv('sampleSubmission.csv')
+    test_df = pd.read_json('test.json')
 
     YCOL = u'requester_received_pizza'
 
@@ -55,10 +62,53 @@ def load_data():
     XCOLS_TOSS = [u'number_of_downvotes_of_request_at_retrieval', u'number_of_upvotes_of_request_at_retrieval', u'post_was_edited', u'request_number_of_comments_at_retrieval', u'request_text', u'requester_account_age_in_days_at_retrieval', u'requester_days_since_first_post_on_raop_at_retrieval', u'requester_number_of_comments_at_retrieval', u'requester_number_of_comments_in_raop_at_retrieval', u'requester_number_of_posts_at_retrieval', u'requester_number_of_posts_on_raop_at_retrieval', u'requester_upvotes_minus_downvotes_at_retrieval', u'requester_upvotes_plus_downvotes_at_retrieval', u'requester_user_flair']
     
     train_df = train_df.drop(labels=XCOLS_TOSS, axis=1)
+
+    for df in train_df, test_df:
+        for c in 'request_text_edit_aware', 'request_title':
+            df[c] = df[c].map(clean_review_function)
+
+    for df in train_df, test_df:
+        for c in 'request_text_edit_aware', 'request_title':
+            print df[c].shape
+
+    print train_df['request_text_edit_aware'].values.shape,\
+                                  test_df['request_text_edit_aware'].values.shape,\
+                                  train_df['request_title'].values.shape,\
+                                  test_df['request_title'].values.shape
+    
+    #train_vectorizer = []
+    #for l in train_df['request_text_edit_aware'].values:
+        #for w in l:
+            #train_vectorizer.append(w)
+    
+    #train_vectorizer = np.array(train_vectorizer)
+    
+    
+    #train_vectorizer = np.concatenate((train_df['request_text_edit_aware'].values, 
+                                  #test_df['request_text_edit_aware'].values,
+                                  #train_df['request_title'].values,
+                                  #test_df['request_title'].values), axis=0)
+
+    train_vectorizer = np.concatenate((train_df['request_text_edit_aware'].values, 
+                                  test_df['request_text_edit_aware'].values,
+                                  train_df['request_title'].values,
+                                  test_df['request_title'].values))
+
+    print train_vectorizer.shape
+
+    nfeatures=1000
+    print 'nfeatures', nfeatures
+    vectorizer = CountVectorizer(analyzer='word', tokenizer=None,  preprocessor=None, stop_words=None, max_features=nfeatures)
+    vectorizer = vectorizer.fit(train_vectorizer)
+    
+    train_df['request_text_edit_aware'] = vectorizer.transform(train_df['request_text_edit_aware'].values).toarray()
+    test_df['request_text_edit_aware'] = vectorizer.transform(test_df['request_text_edit_aware'].values).toarray()
+    train_df['request_title'] = vectorizer.transform(train_df['request_title'].values).toarray()
+    test_df['request_title'] = vectorizer.transform(test_df['request_title'].values).toarray()
     
     for df in train_df, test_df:
-        df['request_text_edit_aware'] = df['request_text_edit_aware'].map(review_to_wordlist).map(len)
-        df['request_title'] = df['request_title'].map(review_to_wordlist).map(len)
+        #df['request_text_edit_aware'] = df['request_text_edit_aware'].map(review_to_wordlist).map(len)
+        #df['request_title'] = df['request_title'].map(review_to_wordlist).map(len)
         df['requester_subreddits_at_request'] = df['requester_subreddits_at_request'].map(len)
         df['requester_username'] = df['requester_username'].map(len)
         df['requester_account_age_in_days_at_request'] = df['requester_account_age_in_days_at_request'].astype(np.int64)
@@ -77,11 +127,11 @@ def load_data():
     xtest = test_df.values[:,2:]
     ytest = test_df.values[:,1]
     
-    #print xtrain.shape, ytrain.shape, xtest.shape, ytest.shape
-    #print xtrain
-    #print ytrain
-    #print xtest
-    #print ytest
+    print xtrain.shape, ytrain.shape, xtest.shape, ytest.shape
+    print xtrain
+    print ytrain
+    print xtest
+    print ytest
     
     return xtrain, ytrain, xtest, ytest
 
@@ -106,14 +156,14 @@ def compare_models(xtraindata, ytraindata):
 
     classifier_dict = {
                 #'linSVC': LinearSVC(),
-                #'kNC5': KNeighborsClassifier(),
-                #'kNC6': KNeighborsClassifier(6),
-                #'SVC': SVC(kernel="linear", C=0.025),
+                'kNC5': KNeighborsClassifier(),
+                'kNC6': KNeighborsClassifier(6),
+                'SVC': SVC(kernel="linear", C=0.025),
                 #'DT': DecisionTreeClassifier(max_depth=5),
-                'RF200': RandomForestClassifier(n_estimators=200, n_jobs=-1),
+                #'RF200': RandomForestClassifier(n_estimators=200, n_jobs=-1),
                 'RF400': RandomForestClassifier(n_estimators=400, n_jobs=-1),
-                'RF800': RandomForestClassifier(n_estimators=800, n_jobs=-1),
-                'RF1000': RandomForestClassifier(n_estimators=1000, n_jobs=-1),
+                #'RF800': RandomForestClassifier(n_estimators=800, n_jobs=-1),
+                #'RF1000': RandomForestClassifier(n_estimators=1000, n_jobs=-1),
                 #'Ada': AdaBoostClassifier(),
                 #'Gauss': GaussianNB(),
                 #'LDA': LDA(),
@@ -144,7 +194,9 @@ def prepare_submission(model, xtrain, ytrain, xtest, ytest):
 if __name__ == '__main__':
     xtrain, ytrain, xtest, ytest = load_data()
     
-    #pca = PCA()
+    print xtrain.shape, ytrain.shape, xtest.shape, ytest.shape
+    
+    #pca = PCA(n_components=10)
     #x_pca = np.vstack([xtrain, xtest])
     #print x_pca.shape
     #pca.fit(xtrain)
@@ -152,9 +204,9 @@ if __name__ == '__main__':
     #xtrain = pca.transform(xtrain)
     #xtest = pca.transform(xtest)
     
-    #compare_models(xtrain, ytrain)
-    model = RandomForestClassifier(n_estimators=400, n_jobs=-1)
-    print 'score', score_model(model, xtrain, ytrain)
-    print model.feature_importances_
-    prepare_submission(model, xtrain, ytrain, xtest, ytest)
+    compare_models(xtrain, ytrain)
+    #model = RandomForestClassifier(n_estimators=800, n_jobs=-1)
+    #print 'score', score_model(model, xtrain, ytrain)
+    #print model.feature_importances_
+    #prepare_submission(model, xtrain, ytrain, xtest, ytest)
     
--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install -y ipython python-matplotlib python-sklearn python-pandas
+
+scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_pizza/*.zip .
+
+for F in *.zip;
+do
+    unzip -x $F;
+done
