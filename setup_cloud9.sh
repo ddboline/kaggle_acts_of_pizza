8c7dfc1133c2aa580d7a9a28bbdfb28e64d031a0
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-02 12:37:30 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
 sudo apt-get install -y ipython python-matplotlib python-sklearn python-pandas unzip
-
-scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_acts_of_pizza/*.zip .
-
-for F in *.zip;
-do
-    unzip -x $F;
-done
-
-./my_model.py
-
-ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk DONE"
