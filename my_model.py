558cfeafd65bc5d7cea7e74a26822d6cc2d76f0a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-29 00:26:11 -0400

    reindent

--- my_model.py
+++ my_model.py
@@ -62,9 +62,9 @@ def load_data():
     YCOL = u'requester_received_pizza'
 
     XCOLS_KEEP = [u'requester_account_age_in_days_at_request', u'requester_days_since_first_post_on_raop_at_request', u'requester_number_of_comments_at_request', u'requester_number_of_comments_in_raop_at_request', u'requester_number_of_posts_at_request', u'requester_number_of_posts_on_raop_at_request', u'requester_number_of_subreddits_at_request', u'requester_upvotes_minus_downvotes_at_request', u'requester_upvotes_plus_downvotes_at_request', u'unix_timestamp_of_request', u'unix_timestamp_of_request_utc']
-    
+
     XCOLS_TOSS = [u'number_of_downvotes_of_request_at_retrieval', u'number_of_upvotes_of_request_at_retrieval', u'post_was_edited', u'request_number_of_comments_at_retrieval', u'request_text', u'requester_account_age_in_days_at_retrieval', u'requester_days_since_first_post_on_raop_at_retrieval', u'requester_number_of_comments_at_retrieval', u'requester_number_of_comments_in_raop_at_retrieval', u'requester_number_of_posts_at_retrieval', u'requester_number_of_posts_on_raop_at_retrieval', u'requester_upvotes_minus_downvotes_at_retrieval', u'requester_upvotes_plus_downvotes_at_retrieval', u'requester_user_flair', u'request_id', u'giver_username_if_known']
-    
+
     train_df = train_df.drop(labels=XCOLS_TOSS, axis=1)
 
     use_text_data = True
@@ -86,12 +86,12 @@ def load_data():
         test_review_features = vectorizer.transform(clean_test_review).toarray()
         train_title_features = vectorizer.transform(clean_train_title).toarray()
         test_title_features = vectorizer.transform(clean_test_title).toarray()
-        
+
         print 'shape0', train_review_features.shape, test_review_features.shape, train_title_features.shape, test_title_features.shape
-    
+
     train_df = train_df.drop(labels=['request_text_edit_aware', 'request_title'], axis=1)
     test_df = test_df.drop(labels=['request_text_edit_aware', 'request_title'], axis=1)
-    
+
     for df in train_df, test_df:
         #df['request_text_edit_aware'] = df['request_text_edit_aware'].map(review_to_wordlist).map(len)
         #df['request_title'] = df['request_title'].map(review_to_wordlist).map(len)
@@ -101,17 +101,17 @@ def load_data():
         df['requester_days_since_first_post_on_raop_at_request'] = df['requester_days_since_first_post_on_raop_at_request'].astype(np.int64)
 
     ytrain = train_df['requester_received_pizza'].astype(np.int64).values
-    
+
     train_df = train_df.drop(labels=['requester_received_pizza'], axis=1)
 
     for c in train_df.columns:
         if train_df[c].dtype == np.int64:
             train_df[c] = train_df[c].astype(np.float64)
             test_df[c] = test_df[c].astype(np.float64)
-    
+
     if use_text_data:
         print 'shape1', train_df.values[:,2:].shape, train_review_features.shape, train_title_features.shape
-        
+
         xtrain = np.hstack([train_df.values[:,2:], train_review_features, train_title_features])
         xtest = np.hstack([test_df.values[:,2:], test_review_features, test_title_features])
         ytest = test_df.values[:,1]
@@ -119,9 +119,9 @@ def load_data():
         xtrain = train_df.values[:,2:]
         xtest = test_df.values[:,2:]
         ytest = test_df.values[:,1]
-    
+
     print 'shape2', xtrain.shape, ytrain.shape, xtest.shape, ytest.shape
-    
+
     return xtrain, ytrain, xtest, ytest
 
 def score_model(model, xtrain, ytrain):
@@ -163,7 +163,7 @@ def compare_models(xtraindata, ytraindata):
                 'LDA': LDA(),
                 #'QDA': QDA(),
                 'SVC': SVC(),
-                }
+               }
 
     results = {}
     ytrain_vals = []
@@ -185,11 +185,11 @@ def compare_models(xtraindata, ytraindata):
         ytest_vals.append(ytpred)
         print name, results[name], ytest_vals[-1]
     print '\n\n\n'
-    
+
     print 'shape3', xTrain.shape, xTest.shape, ytrain_vals[0].shape, ytest_vals[0].shape
     xTrain = np.hstack([xTrain]+[y.reshape(xTrain.shape[0],1) for y in ytrain_vals])
     xTest = np.hstack([xTest]+[y.reshape(xTest.shape[0],1) for y in ytest_vals])
-    
+
     print '\n\n\n'
     model = RandomForestClassifier(n_estimators=400, n_jobs=-1)
     model.fit(xTrain, yTrain)
@@ -200,21 +200,20 @@ def prepare_submission(model, xtrain, ytrain, xtest, ytest):
     model.fit(xtrain, ytrain)
     ytest2 = model.predict(xtest)
     request_id = ytest
-    
+
     df = pd.DataFrame({'request_id': request_id, 'requester_received_pizza': ytest2}, columns=('request_id','requester_received_pizza'))
     df.to_csv('submission.csv', index=False)
-    
+
     return
 
 if __name__ == '__main__':
     xtrain, ytrain, xtest, ytest = load_data()
-    
+
     print 'shapes', xtrain.shape, ytrain.shape, xtest.shape, ytest.shape
-   
+
     compare_models(xtrain, ytrain)
-    #model = Pipeline([('scale', StandardScaler()), 
+    #model = Pipeline([('scale', StandardScaler()),
                       #('rf800', RandomForestClassifier(n_estimators=800, n_jobs=-1))])
     #print 'score', score_model(model, xtrain, ytrain)
     ##print model.feature_importances_
     #prepare_submission(model, xtrain, ytrain, xtest, ytest)
-    
